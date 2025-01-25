5cce245f484800f680648ee7b01754dbb05c0915
Harmen Weber <harmen.weber@gmail.com>
2025-01-26 00:30:42 +0100

    Added placeholders for further templates

---
+++ interactive-options-template.sh
@@ -0,0 +1,8 @@
+# To be written:
+# A script with a mandatory option or argument.
+# If the option/argument is not provided, the scripts asks for it.
+# The script may ask for options with the built-in `select` command.
+# For some free test, the script prompts the user.
+
+echo "To be written." >&2
+exit 1
---
+++ use-config-file-template.sh
@@ -0,0 +1,8 @@
+# To be written:
+# A template for a script that reads a config file from a standard location.
+# However, the location can be customised using an option to the command.
+# The config file doesn't have to set all variables. If a variable is not set
+# in the config file, then the script should use a default value.
+
+echo "To be written." >&2
+exit 1
